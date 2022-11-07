d12da3d40a1743fe9fd7db27e92c562b3372acf7
HerrViseron <daniel-bremen@gmx.de>
2022-11-07 17:34:20 +0100

    Main Bot File creation

---
+++ index.js
@@ -0,0 +1,20 @@
+// Require dotenv for loading the config vomr tne .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+// This Bot uses and .env file for configuration, but the config.json line is still here
+// const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
