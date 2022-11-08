e78f02aec3f690182c491035bdbed2843b5ca419
HerrViseron <daniel-bremen@gmx.de>
2022-11-08 17:21:39 +0100

    NitrAPI command added

--- commands/server.js
+++ commands/server.js
@@ -13,6 +13,7 @@ module.exports = {
 					option
 						.setName('serverid')
 						.setDescription('The ServerID to display info for.')
+						.setRequired(true),
 				),
 		)
 		.addSubcommand(subcommand =>
@@ -23,6 +24,7 @@ module.exports = {
 					option
 						.setName('serverid')
 						.setDescription('The ServerID to start.')
+						.setRequired(true),
 				),
 		)
 		.addSubcommand(subcommand =>
@@ -33,20 +35,26 @@ module.exports = {
 					option
 						.setName('serverid')
 						.setDescription('The ServerID to stop.')
+						.setRequired(true),
 				),
 		),
 	async execute(interaction) {
-		if (interaction.options.getSubcommands() === 'info') {
-			const serverID ) interaction.options.getInteger('serverid')
-		}
-		else if (interaction.options.getSubcommands() === 'start') {
+		await interaction.deferReply();
 
-		}
-		else if (interaction.options.getSubcommands() === 'stop') {
+		// since the ID is always required we can directly retriev it
+		const serverID = interaction.options.getInteger('serverid');
 
+		if (interaction.options.getSubcommand() === 'info') {
+			const infoResult = await request(`https://api.nitrado.net/services/${serverID}/gameservers`, { headers: { authorization: process.env.NITRAPI_TOKEN } });
+			const { data: { status, ip, port, query_port, game, game_human } } = await infoResult.body.json();
+			await interaction.editReply(`Server Status: ${status}, Server IP: ${ip}, Server Port: ${port}, Server Query Port: ${query_port}, Server Game: ${game}, Server Game Human: ${game_human}`);
+		}
+		else if (interaction.options.getSubcommand() === 'start') {
+			await interaction.editReply('Command is still WIP!');
+		}
+		else if (interaction.options.getSubcommand() === 'stop') {
+			await interaction.editReply('Command is still WIP!');
 		}
 
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply({ content: `You are on ${interaction.guild.name}, which has ${interaction.guild.memberCount} members.`, ephemeral: true });
 	},
 };
