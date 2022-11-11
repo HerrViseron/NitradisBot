51b0210504ff84b26abce898f99ae6492f760b99
HerrViseron <daniel-bremen@gmx.de>
2022-11-11 19:20:13 +0100

    not needed anymore

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Ensuring that folder permissions are correct..."
-chown -R node:node /var/lib/nitradisbot
-chown -R node:node /usr/src/nitradisbot
-
-echo "Continuing with App start!"
-exec "$@"
