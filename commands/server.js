816ca332641564460f98aa8ae445cfbb109ae91d
Daniel <admin@viseron.de>
2024-02-04 12:26:28 +0100

    Some testing

--- commands/server.js
+++ commands/server.js
@@ -282,12 +282,12 @@ module.exports = {
 					{ name: 'Game Port', value: `${port}`, inline: true },
 					{ name: 'Query Port', value: `${query_port}`, inline: true },
 				);
-				
+
 				//Some Server specific query information, only for selected games!
 				const games_with_query = ["valheim"]
 				if(games_with_query.includes(game)) {
 					const { data: { gameserver: { game_specific:{ query } } } } = jsonResult;
-
+					console.log(query)
 					serverInfo.addFields(
 						{name: 'Server Name:', value: `${query.server_name}`, inline: true },
 						{name: 'Players Online:', value: `${query.player_current}`, inline: true },
