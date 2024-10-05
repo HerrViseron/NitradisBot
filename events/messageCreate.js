dbb45666ac6f24b13b849878b88e6a083258b33f
Daniel <admin@viseron.de>
2024-10-05 12:14:52 +0200

    Some better returning (with Logging)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,8 @@
 const { Events } = require('discord.js');
 
+const CHANNEL_ID_DND_SHEET_GEN = '1292056999957237802'; // Channel ID in der auf DnD Sheet Dateien reagiert werden soll
+const ALLOWED_MIME_TYPES_DND_SHEET_GEN = ['application/json']; // Allowed MIME-Types for DnD-Sheet-Gen
+
 // Event Listener for Bot command interaction
 // if interaction is not a command -> return an do nothing
 // if command is not log to error console and do nothing
@@ -10,22 +13,31 @@ module.exports = {
 	async execute(message) {
 	/* ### Verarbeite hochgeladene JSON Dateien für den DnD-Sheet-Generator ### */	  
         // Überprüfe, ob die Nachricht im gewünschten Channel gesendet wurde
-        if (message.channel.id !== "1292056999957237802") return;
+        if (message.channel.id !== CHANNEL_ID_DND_SHEET_GEN) return;
 
         // Überprüfe, ob die Nachricht Anhänge enthält
-        if (message.attachments.size > 0) {
-            // Hole die Datei-Anhänge aus der Nachricht
-            message.attachments.forEach(attachment => {
-                // Hier kannst du den Link zur Datei verwenden
-                console.log(`Datei hochgeladen: ${attachment.url}`);
-        
-                // Beispiel: Du kannst die Datei herunterladen oder weiterverarbeiten
-                // Dafür brauchst du ggf. Bibliotheken wie `node-fetch` oder `axios`, um die Datei herunterzuladen
-        
-                message.reply(`Danke für das Hochladen der Datei: ${attachment.name}`);
-                //message.channel.send(`Danke für das Hochladen der Datei: ${attachment.name}`);
-            });
+        if (message.attachments.size <= 0) {
+            console.log(`No Attachement`);
+            return;
         }
+        // Hole die Datei-Anhänge aus der Nachricht
+        message.attachments.forEach(attachment => {
+            
+            if (!ALLOWED_MIME_TYPES_DND_SHEET_GEN.includes(attachement.contentType)) {
+                console.log(`No allowed MIME-Type`);
+                return;
+            }
+
+            // Hier kannst du den Link zur Datei verwenden
+            console.log(`Datei hochgeladen: ${attachment.url}`);
+    
+            // Beispiel: Du kannst die Datei herunterladen oder weiterverarbeiten
+            // Dafür brauchst du ggf. Bibliotheken wie `node-fetch` oder `axios`, um die Datei herunterzuladen
+    
+            message.reply(`Danke für das Hochladen der Datei: ${attachment.name}`);
+            //message.channel.send(`Danke für das Hochladen der Datei: ${attachment.name}`);
+        });
+
     /* ### DnD-Sheet-Gen ENDE ### */
 
 	},
