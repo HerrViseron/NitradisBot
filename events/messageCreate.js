125331cf57d088a612f76ff78c6dd917c68faa7e
Daniel <admin@viseron.de>
2024-10-05 14:49:16 +0200

    Using Node-Fetch

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,5 @@
 const { Events } = require('discord.js');
-const path = require('node:path');
+const fetch = require('node-fetch');
 
 const { genDnDCharSheet } = require('../functions/dndSheetGen')
 
