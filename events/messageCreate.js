f61cb88a8d3d22c76b4d66a9fa9847621274caf4
Daniel <admin@viseron.de>
2024-10-05 15:04:32 +0200

    Some Mode Async and Node-Fetch v2

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,6 +1,5 @@
 const { Events } = require('discord.js');
-import fetch from 'node-fetch';
-
+const fetch = require('node-fetch');
 const { genDnDCharSheet } = require('../functions/dndSheetGen')
 
 const CHANNEL_ID_DND_SHEET_GEN = '1292056999957237802'; // Channel ID in der auf DnD Sheet Dateien reagiert werden soll
@@ -10,7 +9,7 @@ const ALLOWED_MIME_TYPES_DND_SHEET_GEN = ['application/json']; // Allowed MIME-T
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-	/* ### Convert uploaded JSON Files from FoundryVTT to a DnD Character PDF Sheet ### */	  
+        /* ### Convert uploaded JSON Files from FoundryVTT to a DnD Character PDF Sheet ### */	  
         // Check if Message was send in the right Channel, return otherwise
         if (message.channel.id !== CHANNEL_ID_DND_SHEET_GEN) return;
 
@@ -18,7 +17,7 @@ module.exports = {
         if (message.attachments.size <= 0) return;
 
         // Get Attachments from Message
-        message.attachments.forEach(attachment => {
+        message.attachments.forEach(async attachment => {
             // Check MIME-Type of Attachment, return if not on allowed list
             mimeType = attachment.contentType.split(';')[0].trim(); // Only retrieve the MIME-Type and ignore everything after that
             if (!ALLOWED_MIME_TYPES_DND_SHEET_GEN.includes(mimeType)) {
@@ -29,20 +28,19 @@ module.exports = {
                 return;
             }
 
-            const attachmentResponse = fetch(attachment.url);
-            const attachmentJSON = attachmentResponse.json(); //Get Attachment File and read it as JSON Data
+            const attachmentResponse = await fetch(attachment.url);
+            const attachmentJSON = await attachmentResponse.json(); //Get Attachment File and read it as JSON Data
 
             if (attachmentJSON._stats.systemId != 'dnd5e' || attachmentJSON.type != 'character') {
-                message.delete()
+                await message.delete()
                     .then(message.channel.send(`<@${message.author.id}>, dein Upload scheint leider keine DnD5e Charakter zu enthalten`))
                     .then(msg => console.log(`Deleted message from ${msg.author.username}, no Character Data or Wrong Foundry System`))
                     .catch(console.error);
                 return;
             }
 
-
             // Reply to User and start working on the File
-            message.reply(`Danke für das Hochladen deines Charakters "${attachmentJSON.name}"\nIch kümmere mich jetzt um das Erstellen des Charakter Sheets, bitte hab etwas Geduld. Ich antworte Dir, wenn ich fertig bin.`);
+            await message.reply(`Danke für das Hochladen deines Charakters "${attachmentJSON.name}"\nIch kümmere mich jetzt um das Erstellen des Charakter Sheets, bitte hab etwas Geduld. Ich antworte Dir, wenn ich fertig bin.`);
             // Log the URL to the Uploaded File
             console.log(`File uploaded: ${attachment.url}`);
     
