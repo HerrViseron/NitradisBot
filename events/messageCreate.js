5df6be45266a29fb260bd4ba842fa9c06c8a2fcc
Daniel <admin@viseron.de>
2024-10-05 12:35:03 +0200

    Better Comments and MIME-Type handling

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,33 +3,28 @@ const { Events } = require('discord.js');
 const CHANNEL_ID_DND_SHEET_GEN = '1292056999957237802'; // Channel ID in der auf DnD Sheet Dateien reagiert werden soll
 const ALLOWED_MIME_TYPES_DND_SHEET_GEN = ['application/json']; // Allowed MIME-Types for DnD-Sheet-Gen
 
-// Event Listener for Bot command interaction
-// if interaction is not a command -> return an do nothing
-// if command is not log to error console and do nothing
-// if command execures great!
-// if command fails reply the error to the user
+// Event Listener for Messages send in Discord
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
-	/* ### Verarbeite hochgeladene JSON Dateien für den DnD-Sheet-Generator ### */	  
-        // Überprüfe, ob die Nachricht im gewünschten Channel gesendet wurde
+	/* ### Convert uploaded JSON Files from FoundryVTT to a DnD Character PDF Sheet ### */	  
+        // Check if Message was send in the right Channel, return otherwise
         if (message.channel.id !== CHANNEL_ID_DND_SHEET_GEN) return;
 
-        // Überprüfe, ob die Nachricht Anhänge enthält
-        if (message.attachments.size <= 0) {
-            console.log(`No Attachement`);
-            return;
-        }
-        // Hole die Datei-Anhänge aus der Nachricht
+        // Return if no attachment was uploaded
+        if (message.attachments.size <= 0) return;
+
+        // Get Attachments from Message
         message.attachments.forEach(attachment => {
-            
-            if (!ALLOWED_MIME_TYPES_DND_SHEET_GEN.includes(attachment.contentType)) {
+            // Check MIME-Type of Attachment, return if not on allowed list
+            mimeType = attachment.contentType.split(';')[0].trim(); // Only retrieve the MIME-Type and ignore everything after that
+            if (!ALLOWED_MIME_TYPES_DND_SHEET_GEN.includes(mimeType)) {
                 console.log(`No allowed MIME-Type, uploaded: ${attachment.contentType}`);
                 return;
             }
 
             // Hier kannst du den Link zur Datei verwenden
-            console.log(`Datei hochgeladen: ${attachment.name}`);
+            console.log(`File uploaded: ${attachment.url}`);
     
             // Beispiel: Du kannst die Datei herunterladen oder weiterverarbeiten
             // Dafür brauchst du ggf. Bibliotheken wie `node-fetch` oder `axios`, um die Datei herunterzuladen
