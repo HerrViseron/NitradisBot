986f51134978153f8b7e1dd450c06ce460b05e86
Daniel <admin@viseron.de>
2024-10-05 14:09:05 +0200

    Have to subimt the whole object

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -36,7 +36,7 @@ module.exports = {
             console.log(`File uploaded: ${attachment.url}`);
     
             // The Attachment will be handed over to the Sheet Generator Funktion, the rest is handled over there
-            genDnDCharSheet(attachment, message.author.username, message.channel);
+            genDnDCharSheet(attachment, message.author, message.channel);
 
         });
 
