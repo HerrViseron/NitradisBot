12feaa086e762b91e7b2b8e7bc9a8c5b9d408789
Daniel <admin@viseron.de>
2024-10-06 15:44:27 +0200

    Log jsonData

--- functions/dndSheetGen.js
+++ functions/dndSheetGen.js
@@ -17,7 +17,7 @@ function calculateValue(jsonData, calcFunction) {
         case 'baseProficiency':
             let charLevel = 0;
             const charClasses = jsonData.items.filter(item => item.type === 'class');
-            console.log(`CharClasses: ${charClasses}`);
+            console.log(`CharClasses: ${jsonData}`);
             for (const charClass in charClasses) {
                 console.log(charClass);
                 charLevel += charClass.levels;
