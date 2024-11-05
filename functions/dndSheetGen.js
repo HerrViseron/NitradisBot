cbd067b4e2473399c6adced6c3a1da0a985079c4
Daniel <admin@viseron.de>
2024-11-05 18:54:44 +0100

    Debugging

--- functions/dndSheetGen.js
+++ functions/dndSheetGen.js
@@ -64,18 +64,21 @@ function calculateValue(jsonData, calcFunction) {
                         effect.changes.some(change => change.key === 'system.attributes.ac.calc')
                     )
                 ); //Get all items of char, that have someting which adds to the armor class
-            console.log(charArmorEffectsItems)
+
             if (charArmorEffectsItems.length > 0) {
                 //Actually we still have items here but naming everything correctly is hard, I try my best...
                 const firstArmorEffectItem = charArmorEffectsItems[0]; //There should only be one Effect that is not disables, if there are more, the forst one wins
+                console.log("The Whole Item:");
                 console.log(firstArmorEffectItem);
                 const armorEffects = firstArmorEffectItem.effects.find(effect =>
                     !effect.disabled &&
                     effect.changes.some(change => change.key === 'system.attributes.ac.calc')
                 ); //now we have the effects separated and we have the effect that is enabled and affects the AC
+                console.log("The Armor Effects:");
                 console.log(armorEffects);
                 const firstArmorEffect = armorEffects[0]; //Making sure we just use the fist effect if there are more than one.
                 const armorChange = firstArmorEffect?.changes.find(change => change.key === 'system.attributes.ac.calc'); //Get the right change, there might be more than one change but only one should effect the AC 
+                console.log("The Armor Change:");
                 console.log(armorChange);
                 charArmorClassCalcType = armorChange ? armorChange.value : "default"; //Here we finally have the ArmorClass Calculation Type
             }
