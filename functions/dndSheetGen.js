da81330e3950fbb0cb34f59ca1d4a9cf4601fd2a
Daniel <admin@viseron.de>
2024-11-24 12:51:40 +0100

    Different wording

--- functions/dndSheetGen.js
+++ functions/dndSheetGen.js
@@ -165,7 +165,7 @@ function convertJsonToPdfData(jsonData, pdfFieldMapping) {
             case 'list':
                 let i = 1;
                 console.log(value);
-                for (let listItem in value) {
+                for (const listItem of value) {
                     console.log(i);
                     console.log(field);
                     console.log(field + i);
