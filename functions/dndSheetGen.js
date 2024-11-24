59e225067b1113590af92a579192d8e5b6796085
Daniel <admin@viseron.de>
2024-11-24 17:18:52 +0100

    debug

--- functions/dndSheetGen.js
+++ functions/dndSheetGen.js
@@ -320,6 +320,7 @@ function convertJsonToPdfData(jsonData, pdfFieldMapping) {
                 pdfData[field] = calculateValue(jsonData, calcFunction).toString(); // getting the value with a calculation function and always convert to string
                 break;
             case 'fillMultiple':
+                console.log('fillMultiple');
                 fillMultiple(jsonData, fillFunction); //The value mapping and filling data into the fields is handled in the function itself. Most of the functions here are very specific and only apply to one case!
                 break;
             case 'list':
